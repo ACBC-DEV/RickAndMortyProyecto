d64939f3429cbecb1db749fd3fc62f3b3413f6b3
ACBC <camiloblandon69@gmail.com>
2024-01-24 17:14:04 -0500

    Update width of NavBar component

--- app-rickandmorty/src/components/NavBar.tsx
+++ app-rickandmorty/src/components/NavBar.tsx
@@ -29,7 +29,7 @@ export default function NavBar() {
   }
 
   return (
-    <nav className="flex mx-auto justify-center items-center gap-x-2 h-16 px-4 py-2 border-4 border-white rounded-full w-72 my-4">
+    <nav className="flex mx-auto justify-center items-center gap-x-2 h-16 px-4 py-2 border-4 border-white rounded-full w-80 my-4">
       {NavData.map((i) => (
         <Link
           className="hover:border-2 hover:scale-110 border-white rounded-3xl px-2 py-1 duration-75"
