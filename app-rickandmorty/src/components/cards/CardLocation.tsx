5d4b02c69c26d0b3f3d8cec4b9eba3d56b80e960
ACBC <camiloblandon69@gmail.com>
2024-01-29 20:40:57 -0500

    Refactor CardLocation component to include a link to location details

--- app-rickandmorty/src/components/cards/CardLocation.tsx
+++ app-rickandmorty/src/components/cards/CardLocation.tsx
@@ -1,12 +1,20 @@
 import type { TLocations } from "@/types/Tlocations";
-export default function CardLocation({ name, type, dimension }: TLocations) {
+import Link from "next/link";
+export default function CardLocation({
+  name,
+  type,
+  dimension,
+  id,
+}: TLocations) {
   return (
     <article className="bg-mine-shaft-800 border-2 border-white rounded-3xl p-4 duration-200 hover:scale-110 hover:bg-mine-shaft-950">
-      <h2 className="text-center text-3xl">{name}</h2>
-      <div className="flex gap-2">
-        <p>{type}</p>
-        <p>{dimension}</p>
-      </div>
+      <Link href={`/locations/${id}`}>
+        <h2 className="text-center text-3xl">{name}</h2>
+        <div className="flex gap-2">
+          <p>{type}</p>
+          <p>{dimension}</p>
+        </div>
+      </Link>
     </article>
   );
 }
