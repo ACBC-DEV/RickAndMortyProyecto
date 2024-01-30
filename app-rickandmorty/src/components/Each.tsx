8120f70962f17e6faaf750671e08dec0cb150ebe
ACBC <camiloblandon69@gmail.com>
2024-01-29 20:09:23 -0500

    Add TLocations type and update Each component

--- app-rickandmorty/src/components/Each.tsx
+++ app-rickandmorty/src/components/Each.tsx
@@ -1,14 +1,15 @@
 import type { TCharacters } from "@/types/Tcharacters";
+import { TLocations } from "@/types/Tlocations";
 export default function Each({
   Render,
   data,
 }: {
   Render: React.ElementType;
-  data: [] | TCharacters[];
+  data: [] | TCharacters[] | TLocations[];
 }) {
   return (
     <>
-      {data?.map((i: TCharacters) => (
+      {data?.map((i: TCharacters | TLocations) => (
         <Render key={i} {...i} />
       ))}
     </>
---
+++ app-rickandmorty/src/types/Tlocations.ts
@@ -0,0 +1,10 @@
+//"id": 1,
+// "name": "Earth (C-137)",
+// "type": "Planet",
+// "dimension": "Dimension C-137",
+export type TLocations = {
+  id: number;
+  name: string;
+  type: string;
+  dimension: string;
+};
