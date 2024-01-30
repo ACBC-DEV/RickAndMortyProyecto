66e9629b927c32ce2d587112aa0503fdc776ed76
ACBC <camiloblandon69@gmail.com>
2024-01-30 18:38:18 -0500

    Remove NavBar from layout and add it to page and CharacterV2 components

--- app-rickandmorty/src/app/page.tsx
+++ app-rickandmorty/src/app/page.tsx
@@ -1,3 +1,9 @@
+import NavBar from "@/components/NavBar";
+
 export default async function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
--- app-rickandmorty/src/components/Locations.tsx
+++ app-rickandmorty/src/components/Locations.tsx
@@ -1,14 +1,18 @@
 import { TLocations } from "@/types/Tlocations";
 import Each from "./Each";
 import CardLocation from "./cards/CardLocation";
+import NavBar from "./NavBar";
 
 export default function Locations({ data }: { data: TLocations[] }) {
   return (
-    <section className="grid place-content-center">
-      <h2 className="text-8xl text-center m-14 mb-56">Locations</h2>
-      <div className="grid md:flex md:flex-wrap gap-1 md:items-center justify-center m-2">
-        <Each Render={CardLocation} data={data} />
-      </div>
-    </section>
+    <>
+      <NavBar />
+      <section className="grid place-content-center">
+        <h2 className="text-8xl text-center m-14 mb-32">Locations</h2>
+        <div className="grid md:flex md:flex-wrap gap-1 md:items-center justify-center m-2">
+          <Each Render={CardLocation} data={data} />
+        </div>
+      </section>
+    </>
   );
 }
