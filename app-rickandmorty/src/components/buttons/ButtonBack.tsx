0f93791594441a0e2946a88d1a90f8c6d4403ffe
ACBC <camiloblandon69@gmail.com>
2024-01-30 19:45:01 -0500

    Refactor ButtonBack component to use router.back() instead of router.push()

--- app-rickandmorty/src/components/buttons/ButtonBack.tsx
+++ app-rickandmorty/src/components/buttons/ButtonBack.tsx
@@ -1,15 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import useStore from "@/store/useStore";
 import { useRouter } from "next/navigation";
 export default function ButtonBack() {
   const router = useRouter();
-  const { page } = useStore();
-
   return (
     <Button
       variant="outline"
-      onClick={() => router.push(`/characters?page=${page}`)}
+      onClick={() => router.back()}
       className="w-20 h-10 m-2 rounded-3xl bg- border-2 border-white"
     >
       Back
