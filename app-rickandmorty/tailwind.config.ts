223296d5df50878b850b373601cc3adecdb12363
ACBC <camiloblandon69@gmail.com>
2024-01-30 19:44:28 -0500

    Remove darkMode option and add tailwindcss-animate plugin

--- app-rickandmorty/tailwind.config.ts
+++ app-rickandmorty/tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -101,6 +100,7 @@ const config = {
       },
     },
   },
+
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
