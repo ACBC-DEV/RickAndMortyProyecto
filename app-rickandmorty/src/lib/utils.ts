9c2e56598f9089937f14ac39c7d237a74b7e7fde
ACBC <camiloblandon69@gmail.com>
2024-02-02 18:27:54 -0500

    Agregadas funciones cn y extractSeasonEpisode a utils.ts

--- app-rickandmorty/src/lib/utils.ts
+++ app-rickandmorty/src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function extractSeasonEpisode(
+  str: string
+): { season: number; episode: number } | null {
+  const regex = /S(\d+)E(\d+)/;
+  const match = str?.match(regex);
+
+  if (match) {
+    const season = parseInt(match[1], 10);
+    const episode = parseInt(match[2], 10);
+
+    return { season, episode };
+  }
+
+  return null;
 }
