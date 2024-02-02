c4fda53f90cdc4cabdfd306968424ed505353d17
ACBC <camiloblandon69@gmail.com>
2024-02-02 18:28:00 -0500

    Actualizar componente CardEpisode

--- app-rickandmorty/src/components/Cards/CardEpisode.tsx
+++ app-rickandmorty/src/components/Cards/CardEpisode.tsx
@@ -1,19 +1,6 @@
 import { TEpisode } from "@/types";
-function extractSeasonEpisode(
-  str: string
-): { season: number; episode: number } | null {
-  const regex = /S(\d+)E(\d+)/;
-  const match = str?.match(regex);
-
-  if (match) {
-    const season = parseInt(match[1], 10);
-    const episode = parseInt(match[2], 10);
-
-    return { season, episode };
-  }
-
-  return null;
-}
+import { extractSeasonEpisode } from "@lib/utils";
+import Link from "next/link";
 export default function CardEpisode(props: TEpisode) {
   const result = extractSeasonEpisode(props.episode);
   let season: number | null = null;
@@ -24,15 +11,17 @@ export default function CardEpisode(props: TEpisode) {
     episode = result.episode;
   }
   return (
-    <article className="box duration-200 hover:scale-110 hover:bg-mine-shaft-900 hover:shadow-sm hover:shadow-white">
-      <div className="flex flex-col md:flex-row gap-2 justify-evenly items-center">
-        <h2 className="text-4xl text-center">{props.name}</h2>
-        <div>
-          <p>{season} season</p>
-          <p>{episode} episode</p>
+    <article className="box duration-200  md:hover:scale-110 hover:bg-mine-shaft-900 hover:shadow-sm hover:shadow-white">
+      <Link href={`/episodes/${props.id}`}>
+        <div className="flex flex-col md:flex-row gap-2 justify-evenly items-center">
+          <h2 className="text-4xl text-center">{props.name}</h2>
+          <div className="flex gap-x-2 justify-around items-center md:flex-col">
+            <p>{season} season</p>
+            <p>{episode} episode</p>
+          </div>
         </div>
-      </div>
-      <p className="text-center">{props.air_date}</p>
+        <p className="text-center">{props.air_date}</p>
+      </Link>
     </article>
   );
 }
