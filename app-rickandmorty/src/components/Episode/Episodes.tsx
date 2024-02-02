c4530bfa3bcdb0735d575b22d0ab17d4fdebf045
ACBC <camiloblandon69@gmail.com>
2024-02-01 21:08:17 -0500

    Agrega NavBar y título a la página de episodios

--- app-rickandmorty/src/components/Episode/Episodes.tsx
+++ app-rickandmorty/src/components/Episode/Episodes.tsx
@@ -1,5 +1,12 @@
 import { EpisodesList } from ".";
+import NavBar from "../NavBar";
 
 export default function Episodes() {
-  return <EpisodesList />;
+  return (
+    <>
+      <NavBar />
+      <h2 className="text-4xl text-center">Episodes</h2>
+      <EpisodesList />;
+    </>
+  );
 }
