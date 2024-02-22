837de4623e24e51ee12b9b12d228adb631f30728
ACBC <camiloblandon69@gmail.com>
2024-02-22 14:46:02 -0500

    add new font

--- app-rickandmorty/src/app/layout.tsx
+++ app-rickandmorty/src/app/layout.tsx
@@ -1,29 +1,26 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// Supports weights 100-900
+import "@fontsource-variable/noto-sans-devanagari";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-  title: "Rick and Morty",
-  description:
-    "Interactive Rick and Morty web project offering an in-depth exploration of the series universe, including character profiles, episodes, and locations, with interactive features and exclusive content for fans",
+	title: "Rick and Morty",
+	description:
+		"Interactive Rick and Morty web project offering an in-depth exploration of the series universe, including character profiles, episodes, and locations, with interactive features and exclusive content for fans",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} bg-mine-shaft-950 text-white selection:text-atlantis-400 selection:bg-mine-shaft-700`}
-      >
-        <NavBar />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className="bg-mine-shaft-950 text-white selection:text-atlantis-400 selection:bg-mine-shaft-700">
+				<NavBar />
+				{children}
+			</body>
+		</html>
+	);
 }
