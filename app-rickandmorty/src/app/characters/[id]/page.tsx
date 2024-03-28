340360779ae3e979e08132c183c14b8e94cbe69f
ACBC <acbc.dev@gmail.com>
2024-03-27 19:56:50 -0500

    Refactor CharacterID component

--- app-rickandmorty/src/app/characters/[id]/page.tsx
+++ app-rickandmorty/src/app/characters/[id]/page.tsx
@@ -4,28 +4,28 @@ import { getCharacter } from "@/lib/fetch";
 import ButtonPagination from "@/components/buttons/buttonPagination";
 
 export default async function CharacterID({
-	params,
+  params,
 }: {
-	params: { id: string };
+  params: { id: string };
 }) {
-	const data = await getCharacter(parseInt(params.id));
-	const id = Number(params.id);
+  const data = await getCharacter(parseInt(params.id));
+  const id = Number(params.id);
 
-	return (
-		<section className=" grid place-content-center">
-			<ButtonBack />
-			<Character {...data} />
-			<div className="flex  gap-x-4 mx-3 place-content-center items-center">
-				{id === 1 ? (
-					""
-				) : (
-					<ButtonPagination href={`${id - 1}`}>prev</ButtonPagination>
-				)}
-				<p className="text-center ">{id}</p>
-				<ButtonPagination href={`${id + 1}`}>next</ButtonPagination>
-			</div>
+  return (
+    <section className=" grid place-content-center">
+      <ButtonBack />
+      <Character {...data} />
+      <div className="flex  gap-x-4 mx-3 place-content-center items-center">
+        {id === 1 ? (
+          ""
+        ) : (
+          <ButtonPagination href={`${id - 1}`}>prev</ButtonPagination>
+        )}
+        <p className="text-center ">{id}</p>
+        <ButtonPagination href={`${id + 1}`}>next</ButtonPagination>
+      </div>
 
-			<CharacterGrid />
-		</section>
-	);
+      <CharacterGrid page={id} />
+    </section>
+  );
 }
