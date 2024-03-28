7baaf9d8afd0cd1a3e5bfd06df248aff2ab02690
ACBC <acbc.dev@gmail.com>
2024-03-27 19:57:16 -0500

    Refactor CharacterGrid component

--- app-rickandmorty/src/components/Character/CharacterGrid.tsx
+++ app-rickandmorty/src/components/Character/CharacterGrid.tsx
@@ -4,13 +4,13 @@ import Each from "../Each";
 import PaginationComponent from "../buttons/PaginationComponent";
 
 export default async function CharacterGrid({ page }: { page: number }) {
-	const { results } = await getCharacters(page);
-	return (
-		<>
-			<div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-				<Each Render={CardCharacter} data={results} />
-			</div>
-			<PaginationComponent />
-		</>
-	);
+  const { results, info } = await getCharacters(page);
+  return (
+    <>
+      <div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+        <Each Render={CardCharacter} data={results} />
+      </div>
+      <PaginationComponent page={page || 1} pagination={info} />
+    </>
+  );
 }
