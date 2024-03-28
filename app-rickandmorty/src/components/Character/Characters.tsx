0a3bcb9dcf18f4081c9a640797a1d689d5cae45c
ACBC <acbc.dev@gmail.com>
2024-03-27 19:57:21 -0500

    Refactor Characters component to include pagination information

--- app-rickandmorty/src/components/Character/Characters.tsx
+++ app-rickandmorty/src/components/Character/Characters.tsx
@@ -4,13 +4,13 @@ import { getCharacters } from "@lib/fetch";
 import PaginationComponent from "../buttons/PaginationComponent";
 
 export default async function Characters({ page }: { page: number }) {
-	const { results } = await getCharacters(page);
-	return (
-		<section className="grid place-content-center">
-			<div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-				<Each Render={CardCharacter} data={results} />
-			</div>
-			<PaginationComponent />
-		</section>
-	);
+  const { results, info } = await getCharacters(page);
+  return (
+    <section className="grid place-content-center">
+      <div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+        <Each Render={CardCharacter} data={results} />
+      </div>
+      <PaginationComponent page={page || 1} pagination={info} />
+    </section>
+  );
 }
